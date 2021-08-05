b93d6890352d38496a0e145ec74aa7e033691368
Murat <muradgirgin@gmail.com>
2021-08-05 18:01:42 +0300

    DbContext+CRUD lessons completed

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.DBOperations;
 
 namespace WebApi.AddControllers 
 {
@@ -9,60 +10,70 @@ namespace WebApi.AddControllers
     [Route("[controller]s")]
     public class BookController : ControllerBase
     {
-        private static List<Book> BookList = new List<Book>()
+        // to prevent to be overwritten (readonly) and set in the constructor (private)
+        private readonly BookStoreDBContext _context; 
+
+        public BookController (BookStoreDBContext context)
         {
-            new Book{
-                Id = 1, 
-                Title = "Lean Startup", 
-                GenreId = 1, 
-                PageCount = 250, 
-                PublishDate = new DateTime(2001, 06, 12)
-            },
-            new Book{
-                Id = 2, 
-                Title = "Herland", 
-                GenreId = 2, 
-                PageCount = 300, 
-                PublishDate = new DateTime(2010, 05, 23)
-            }, 
-            new Book{
-                Id = 3, 
-                Title = "Dume", 
-                GenreId = 2, 
-                PageCount = 540, 
-                PublishDate = new DateTime(2001, 12, 21)
-            }
-        }; 
+            _context = context;
+        }
+
+        // private static List<Book> BookList = new List<Book>()
+        // {
+        //     new Book{
+        //         Id = 1, 
+        //         Title = "Lean Startup", 
+        //         GenreId = 1, 
+        //         PageCount = 250, 
+        //         PublishDate = new DateTime(2001, 06, 12)
+        //     },
+        //     new Book{
+        //         Id = 2, 
+        //         Title = "Herland", 
+        //         GenreId = 2, 
+        //         PageCount = 300, 
+        //         PublishDate = new DateTime(2010, 05, 23)
+        //     }, 
+        //     new Book{
+        //         Id = 3, 
+        //         Title = "Dume", 
+        //         GenreId = 2, 
+        //         PageCount = 540, 
+        //         PublishDate = new DateTime(2001, 12, 21)
+        //     }
+        // }; 
 
         [HttpGet]
         public List<Book> GetBooks()
         {
-            var bookList = BookList.OrderBy(x=> x.Id).ToList<Book>();
+            var bookList = _context.Books.OrderBy(x=> x.Id).ToList<Book>();
             return bookList;
         }
         
         [HttpGet("{id}")]
         public Book GetById(int id)
         {
-            var book = BookList.Where(book=> book.Id == id).SingleOrDefault();
+            var book = _context.Books.Where(book=> book.Id == id).SingleOrDefault();
             return book;
         }
 
         [HttpPost]
         public IActionResult AddBook([FromBody] Book newBook) 
         {
-            var book = BookList.SingleOrDefault(x=> x.Title == newBook.Title);
+            var book = _context.Books.SingleOrDefault(x=> x.Title == newBook.Title);
             if (book is not null)
                 return BadRequest();
 
-            BookList.Add(newBook);
+            _context.Books.Add(newBook);
+            _context.SaveChanges(); 
+
             return Ok();
         }
 
         [HttpPut("{id}")]
         public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
         {
-            var book = BookList.SingleOrDefault(x=> x.Id == id); 
+            var book = _context.Books.SingleOrDefault(x=> x.Id == id); 
             if (book is null)
                 return BadRequest(); 
 
@@ -71,18 +82,21 @@ namespace WebApi.AddControllers
             book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate; 
             book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title; 
 
+            _context.SaveChanges();
+
             return Ok();
         } 
 
         [HttpDelete("{id}")]
         public IActionResult DeleteBook(int id) 
         {
-            var book = BookList.SingleOrDefault(x=> x.Id == id); 
+            var book = _context.Books.SingleOrDefault(x=> x.Id == id); 
             if (book is null)
                 return BadRequest();
 
-            BookList.Remove(book);
-
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            
             return Ok(); 
         }
     } 
---
+++ WebApi/DBOperations/DataGenerator.cs
@@ -0,0 +1,47 @@
+using System; 
+using Microsoft.Extensions.DependencyInjection; 
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace WebApi.DBOperations 
+{ 
+    public class DataGenerator 
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookStoreDBContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDBContext>>()))
+            {
+                if (context.Books.Any())
+                {
+                    return;
+                }
+
+                context.Books.AddRange(
+                    new Book{
+                        // Id = 1, 
+                        Title = "Lean Startup", 
+                        GenreId = 1, 
+                        PageCount = 250, 
+                        PublishDate = new DateTime(2001, 06, 12)
+                    },
+                    new Book{
+                        // Id = 2, 
+                        Title = "Herland", 
+                        GenreId = 2, 
+                        PageCount = 300, 
+                        PublishDate = new DateTime(2010, 05, 23)
+                    }, 
+                    new Book{
+                        // Id = 3, 
+                        Title = "Dume", 
+                        GenreId = 2, 
+                        PageCount = 540, 
+                        PublishDate = new DateTime(2001, 12, 21)
+                     }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}

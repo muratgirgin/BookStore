2033c0087764616ae61c2c8b404c169cd6a8b291
Murat <muradgirgin@gmail.com>
2021-08-25 16:04:51 +0300

    services completed till controller

---
+++ WebApi/Application/GenreOperations/Commands/UpdateGenre/UpdateGenreCommand.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using WebApi.DBOperations;
+using WebApi.Entities;
+
+namespace  WebApi.Application.GenreOperations.UpdateGenre 
+{
+    public class UpdateGenreCommand 
+    { 
+        public int GenreId { get; set; }
+        public UpdateGenreModel Model { get; set; }    
+
+        private readonly BookStoreDBContext _context;
+        public UpdateGenreCommand(BookStoreDBContext context)
+        {
+            _context = context;
+        }
+
+        public void Handle() 
+        {
+            var genre = _context.Genres.SingleOrDefault(x => x.Id == GenreId); 
+            if (genre is null) 
+                throw new InvalidOperationException("GenreId not found");
+            
+            if (_context.Genres.Any(x => x.Name.ToLower() == Model.Name.ToLower() && x.Id != GenreId))
+                throw new InvalidOperationException("Genre already exists with this name");
+
+            genre.Name = Model.Name.Trim() == default ? Model.Name : genre.Name;
+            genre.IsActive = Model.IsActive;
+
+            _context.SaveChanges(); 
+        }
+    }
+
+    public class UpdateGenreModel
+    {
+        public string Name { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}

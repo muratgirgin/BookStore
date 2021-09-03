ddfacb64668ed2ed65c1eea4c9a8348a7da62dd7
Murat <muradgirgin@gmail.com>
2021-09-03 21:12:25 +0300

    CommonTestFixture and TestData classes completed

---
+++ Tests/WebApi.UnitTests/TestSetup/Books.cs
@@ -0,0 +1,18 @@
+using System;
+using WebApi.DBOperations;
+using WebApi.Entities;
+
+namespace TestSetup
+{
+    public static class Books
+    {
+        public static void AddBooks(this BookStoreDBContext context)
+        {
+            context.Books.AddRange(
+                new Book { Title = "Lean Startup", GenreId = 1, PageCount = 250, PublishDate = new DateTime(2001, 06, 12) },
+                new Book { Title = "Herland", GenreId = 2, PageCount = 300, PublishDate = new DateTime(2010, 05, 23) },
+                new Book { Title = "Dume", GenreId = 2, PageCount = 540, PublishDate = new DateTime(2001, 12, 21) }
+            );
+        }
+    }
+}

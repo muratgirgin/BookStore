b31cfe729ccbfc94c8d5f27012941311b2b3917e
Murat <muradgirgin@gmail.com>
2021-09-29 16:32:21 +0300

    Test #1 for CreateBookCommand

--- Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateBook/CreateBookCommandTests.cs
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateBook/CreateBookCommandTests.cs
@@ -1,7 +1,41 @@
+using System;
+using AutoMapper;
+using FluentAssertions;
+using TestSetup;
+using WebApi.Application.BookOperations.Commands.CreateBook;
+using WebApi.DBOperations;
+using WebApi.Entities;
+using Xunit;
+using static WebApi.Application.BookOperations.Commands.CreateBook.CreateBookCommand;
+
 namespace Application.BookOperations.Commands.CreateCommand
 {
-    public class CreateBookCommandTests
+    public class CreateBookCommandTests : IClassFixture<CommonTestFixture>
     {
+        private readonly BookStoreDBContext _context;
+        private readonly IMapper _mapper;
 
+        public CreateBookCommandTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.Context;
+            _mapper = testFixture.Mapper;
+        }
+
+        [Fact]
+        public void WhenAlreadyExistsBookTitleIsGiven_InvalidOperationException_ShouldBeReturned()
+        {
+            //arrange 
+            var book = new Book() { Title = "WhenAlreadyExistsBookTitleIsGiven_InvalidOperationException_ShouldBeReturned", PageCount = 100, PublishDate = new System.DateTime(1983, 01, 10), GenreId = 1 };
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            CreateBookCommand command = new CreateBookCommand(_context, _mapper);
+            command.Model = new CreateBookModel() { Title = book.Title };
+
+            //act & assert
+            FluentActions
+                .Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Book already exists");
+        }
     }
 }

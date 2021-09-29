f218bcba4f7fdffdd2b5e4f7e7cd76f96602a548
Murat <muradgirgin@gmail.com>
2021-09-29 17:12:20 +0300

    Test #2 for CreateBookValidator completed and happy path scenarios added

--- Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateBook/CreateBookCommandTests.cs
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateBook/CreateBookCommandTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using AutoMapper;
 using FluentAssertions;
 using TestSetup;
@@ -8,7 +9,7 @@ using WebApi.Entities;
 using Xunit;
 using static WebApi.Application.BookOperations.Commands.CreateBook.CreateBookCommand;
 
-namespace Application.BookOperations.Commands.CreateCommand
+namespace Application.BookOperations.Commands.CreateBook
 {
     public class CreateBookCommandTests : IClassFixture<CommonTestFixture>
     {
@@ -22,20 +23,40 @@ namespace Application.BookOperations.Commands.CreateCommand
         }
 
         [Fact]
-        public void WhenAlreadyExistsBookTitleIsGiven_InvalidOperationException_ShouldBeReturned()
+        public void WhenAlreadyExistsBookTitleIsGiven_InvalidOperationException_ShouldReturn()
         {
-            //arrange 
-            var book = new Book() { Title = "WhenAlreadyExistsBookTitleIsGiven_InvalidOperationException_ShouldBeReturned", PageCount = 100, PublishDate = new System.DateTime(1983, 01, 10), GenreId = 1 };
+            // arrange 
+            var book = new Book() { Title = "WhenAlreadyExistsBookTitleIsGiven_InvalidOperationException_ShouldReturn", PageCount = 100, PublishDate = new System.DateTime(1983, 01, 10), GenreId = 1 };
             _context.Books.Add(book);
             _context.SaveChanges();
 
             CreateBookCommand command = new CreateBookCommand(_context, _mapper);
             command.Model = new CreateBookModel() { Title = book.Title };
 
-            //act & assert
+            // act & assert
             FluentActions
                 .Invoking(() => command.Handle())
                 .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Book already exists");
+        }
+
+        [Fact]
+        public void WhenValidInputsAreGiven_BookShouldBeCreated()
+        {
+            // arrange 
+            CreateBookCommand command = new CreateBookCommand(_context, _mapper);
+            CreateBookModel model = new CreateBookModel() { Title = "Hobbit", PageCount = 250, PublishDate = DateTime.Now.Date.AddYears(-20), GenreId = 1 };
+            command.Model = model;
+
+            // act 
+            FluentActions.Invoking(() => command.Handle()).Invoke();
+
+            // assert 
+            var book = _context.Books.SingleOrDefault(book => book.Title == model.Title);
+
+            book.Should().NotBeNull();
+            book.PageCount.Should().Be(model.PageCount);
+            book.PublishDate.Should().Be(model.PublishDate);
+            book.GenreId.Should().Be(model.GenreId);
         }
     }
 }
